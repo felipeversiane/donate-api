e7dc67ebbe76899eafeeaa0f1ef026f572042665
Felipe Fernandes Versiane <felipeversiane09@gmail.com>
2024-09-13 08:59:33 -0300

    refactor: refactoring router

---
+++ internal/infra/server/server.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/felipeversiane/donate-api/internal/infra/config"
+	"github.com/felipeversiane/donate-api/internal/infra/database"
+	"github.com/gin-gonic/gin"
+)
+
+type ServerInterface interface {
+	SetupRouter()
+	Start()
+}
+
+type Server struct {
+	router   *gin.Engine
+	config   config.ServerConfig
+	database database.DatabaseInterface
+}
+
+func NewServer(
+	cfg config.ServerConfig,
+	database database.DatabaseInterface,
+) ServerInterface {
+	server := &Server{
+		router:   gin.New(),
+		config:   cfg,
+		database: database,
+	}
+	return server
+}
+
+func (s *Server) SetupRouter() {
+	s.router.Use(gin.Recovery())
+	s.router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "healthy",
+		})
+	})
+}
+
+func (s *Server) Start() {
+	port := s.config.Port
+	if port == "" {
+		port = "8000"
+	}
+	err := s.router.Run(fmt.Sprintf(":%s", port))
+	if err != nil {
+		slog.Error("failed to start server", slog.Any("error", err))
+	}
+}
